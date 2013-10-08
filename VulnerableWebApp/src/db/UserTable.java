488a72a3136b1253aa872c5d456d2c8591796f7e
Michael Reif <mreif@mathematik.uni-marburg.de>
2013-10-08 18:59:46 +0200

    changed user validation

--- VulnerableWebApp/src/db/UserTable.java
+++ VulnerableWebApp/src/db/UserTable.java
@@ -4,6 +4,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
+import util.Security;
 import dao.User;
 
 public class UserTable extends TableQuery<User> {
@@ -68,9 +69,9 @@ public class UserTable extends TableQuery<User> {
 				for(int i = 1 ; i <= rs.getMetaData().getColumnCount(); i++){
 					String data = rs.getObject(i).toString();
 					switch (i){
-						case 3: password = data;
-						case 2: username = data;
-						case 1: element_id = new Integer(data).intValue();
+						case 3: password = data; break;
+						case 2: username = data; break;
+						case 1: element_id = new Integer(data).intValue(); break;
 					}
 				}
 				user = new User(element_id, username, password);
@@ -89,36 +90,39 @@ public class UserTable extends TableQuery<User> {
 	 * 		   1 if username is incorrect
 	 * 	  	   2 if password is incorrect
 	 */
-	public int validateUser(User user){
-		ArrayList<User> users = this.getAll();
-		boolean userc = false;
-		boolean pwc = false;
-		User current = null;
+	public int validateUser(String name, String unhashedPassword){
 		
-		for(int i = 0; i < users.size(); i++){
-			current = users.get(i);
-			if(current.getUsername().equals(user.getUsername())){
-				userc=true;
-				if(current.getPassword().equals(user.getPassword())){
-					pwc = true;
+		String query = String.format("SELECT * FROM %s WHERE username=%s;", this.getTable(), name);
+		ResultSet rs = this.executeQuery(query);
+
+		String username = null;
+		String password = null;
+		User user = null;
+		int result = 1;
+
+		
+		try {
+			while(rs.next()){
+				for(int i = 1 ; i <= rs.getMetaData().getColumnCount(); i++){
+					String data = rs.getObject(i).toString();
+					switch (i){
+						case 3: password = data; break;
+						case 2: username = data; break;
+					}
 				}
 			}
+		} catch (SQLException e) {
+			QueryLogger.getInstance().logError("user, validateUser()");
 		}
 		
-		return (userc)? (pwc)? 0 : 2 : 1;
-	}
-	
-	/**
-	 * Validates user for login.
-	 * @param user to validate
-	 * @return 0 if exists
-	 * 		   1 if username is incorrect
-	 * 	  	   2 if password is incorrect
-	 */
-	public int validateUser(String name, String unhashedPassword){
-		User user = new User(-1, name, unhashedPassword);
-		user.hashPassword();
-		return validateUser(user);
+		if(username != null){
+			user = new User(-1, username, password);
+			String hash = Security.md5(unhashedPassword);
+			
+			result = (user.getPassword().equals(hash))? 0 : 2;
+		}
+		
+		return result;
 	}
 	
 	public void printTable(ArrayList<User> users) {
